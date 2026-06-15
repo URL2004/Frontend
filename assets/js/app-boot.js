3371c6d5aae6238898192de2303d93806e3e5462
ecobahn <ecobahn.sba@gmail.com>
2026-06-15 09:46:51 +0900

    fix: simplify evasion choice copy

--- assets/js/app-boot.js
+++ assets/js/app-boot.js
@@ -4,7 +4,7 @@
     return;
   }
 
-  var v = 'lav-116';
+  var v = 'lav-117';
   function script(src, attrs) {
     attrs = attrs || '';
     document.write('<script ' + attrs + ' src="' + src + '"><\/script>');
